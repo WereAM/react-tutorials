fa7bcd664ef2680b74748a171771a5b091149365
WereAM <michelle.ambunya@strathmore.edu>
2024-04-01 17:18:47 +0300

    feat: Writing markup with JSX
    Created AboutPage component
    Wrapped the JSX tags in a wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import MyButton from './components/MyButton';
+import AboutPage from './components/AboutPage';
 
 export default function App() {
   return (
     <div>
-      <h1> Welcome to my app</h1>
-      <MyButton/>
+      {/* <h1> Welcome to my app</h1>
+      <MyButton/> */}
+
+      <AboutPage/>
     </div>
   );
 }
